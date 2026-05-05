362f38fbb179d36553685a8aebd6f8585721de5f
vikalp <chaurasiavikalp1996@gmail.com>
2026-05-06 01:02:34 +0530

    final model initial commit

--- helper.py
+++ helper.py
@@ -11,3 +11,123 @@ def assign(left, right):
     if left.shape != right.shape:
         raise ValueError(f"Shape mismatch: {left.shape} vs {right.shape}")
     return torch.nn.Parameter(torch.tensor(right))
+
+def load_weight_into_gpt(gpt, params):
+    gpt.pos_emb_weight = assign(gpt.pos_emb_weight, params["wpe"])
+    gpt.tok_emb_weight = assign(gpt.token_emb_weight, params["wte"]) 
+
+    for b in range(len(params["blocks"])):
+        q_w,k_w,v_w = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].attn.W_query.weight = assign(
+            gpt.trf_blocks[b].attn.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].attn.W_key.weight = assign(
+            gpt.trf_blocks[b].attn.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].attn.W_value.weight = assign(
+            gpt.trf_blocks[b].attn.W_value.weight, v_w.T)
+        
+        q_b,k_b,v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].attn.W_query.bias = assign(
+            gpt.trf_blocks[b].attn.W_query.bias, q_b)
+        gpt.trf_blocks[b].attn.W_key.bias = assign(
+            gpt.trf_blocks[b].attn.W_key.bias, k_b)
+        gpt.trf_blocks[b].attn.W_value.bias = assign(
+            gpt.trf_blocks[b].attn.W_value.bias, v_b)
+        
+        gpt.trf_blocks[b].attn.out_proj.weight = assign(
+            gpt.trf_blocks[b].attn.out.weight, (
+            params["blocks"][b]["attn"]["c_proj"])["w"].T)
+        gpt.trf_blocks[b].attn.out_proj.bias = assign(
+            gpt.trf_blocks[b].attn.out_proj.bias, (
+            params["blocks"][b]["attn"]["c_proj"])["b"])
+        
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight, (
+            params["blocks"][b]["mlp"]["c_fc"])["w"].T) 
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias, (
+            params["blocks"][b]["mlp"]["c_fc"])["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight, (
+            params["blocks"][b]["mlp"]["c_proj"])["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias, (  
+            params["blocks"][b]["mlp"]["c_proj"])["b"])
+        
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale, (
+            params["blocks"][b]["ln_1"]["g"]))
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift, (
+            params["blocks"][b]["ln_1"]["b"]))
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale, (
+            params["blocks"][b]["ln_2"]["g"]))
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift, (
+            params["blocks"][b]["ln_2"]["b"]))
+        
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+def load_gpt2_params_from_tf_ckpt(ckpt_path,settings):
+    params = {"blocks": [{} for _ in range(settings["n_layer"])]}
+        
+    for name,_ in tf.train.list_variables(ckpt_path):
+        variable_array = np.squeeze(tf.train.load_variable(ckpt_path, mane))
+        variable_name_parts = name.split("/")[1:]
+
+        target_dict = params
+        if variable_name_parts[0].startswith("h"):
+            layer_number = int(variable_name_parts[0][1:])
+            target_dict = params["blocks"][layer_number]
+
+        for key in variable_name_parts[1:-1]:
+            target_dict = target_dict.setdefault(key, {})
+
+        last_key = variable_name_parts[-1]
+        target_dict[last_key] = variable_array
+
+    total_params = count_params(params)
+   
+    print(f"Total parameters in file: {total_params}")
+   
+    return params
+
+def count_params(d):
+    total = 0
+    for v in d.values():
+        if isinstance(v, dict):
+            total += count_params(v)
+        elif isinstance(v, list):
+            for item in v:
+              total += count_params(item)
+        else:
+            total += v.size
+    return total
+
+
+def get_settings_and_params(model_dir):
+    tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+    settings = json.load(open(os.path.join(model_dir, "hparams.json"), "r", encoding="utf-8"))
+    params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, settings)
+
+    return settings, params
+
+def debug_parameters(model):
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total Trainable parameters: {trainable_params}")
+    print(f"Total parameters: {total_params}")
+
+    param_counts = defaultdict(int)
+
+    for name, param in model.named_parameters():
+        top_level = name.split('.')[0]
+        param_counts[top_level] += param.numel()
+
+    for k, v in param_counts.items():
+        print(f"{k}: {v} parameters")    
+           
--- model.py
+++ model.py
@@ -6,3 +6,69 @@ import torch.nn.functional as F
 import tiktoken
 
 from helper import debug_parameters, get_settings_and_params, load_weight_into_gpt 
+
+MODEL_NAME ="355M"
+MODEL_DIR = os.path.join("models", MODEL_NAME)
+
+
+if MODEL_NAME == "124M":
+    VOCAB_SIZE = 50257
+    CONTEXT_LEN = 1024
+    NUM_HEADS = 12
+    NUM_LAYER = 12
+    EMB_DIM = 768
+    DROP_RATE = 0.1
+    QKV_BIAS = True
+elif MODEL_NAME == "355M":
+    VOCAB_SIZE = 50257
+    CONTEXT_LEN = 1024
+    NUM_HEADS = 20
+    NUM_LAYER = 24
+    EMB_DIM = 1280
+    DROP_RATE = 0.1
+    QKV_BIAS = True
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out,
+                 CONTEXT_LEN, dropout,num_heads, qkv_bias = False):
+        super().__init__()
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask", torch.triu(torch.ones(CONTEXT_LEN, CONTEXT_LEN), diagonal=1))
+
+        
+    def forward(self, x):
+        b, num_tokens,_ = x.size()
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        attn_scores = attn_scores.masked_fill(mask_bool, torch.inf)
+        attn_weights = torch.softmax(attn_scores/ keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
+      
+        return context_vec
+
+    class LayerNorm(nn.Module):
+       
